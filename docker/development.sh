0c382a6928667cee6a96ae226dc13c9d5320f913
Jens Van den Broeck <jens.vdbroeck@gmail.com>
2018-12-21 14:11:02 +0100

    own modifications

--- docker/development.sh
+++ docker/development.sh
@@ -20,5 +20,13 @@ cd ..
 # Add a volume for docker
 add_volume docker
 
+# add /data volume
+datapath=${HOME}/actc-data
+if [ $(hostname) == "pegasus.elis.ugent.be" ]; then
+  datapath=/bulk/A/measurements
+fi
+export ADDITIONALVOLUMES="$ADDITIONALVOLUMES -v $datapath:/data"
+
 # Run development startup script
-docker-compose run ${ADDITIONALVOLUMES} actc /opt/development/docker/actc/development.sh
+xhost +local:root;
+docker-compose run -e DISPLAY=${DISPLAY} -v /tmp/.X11-unix:/tmp/.X11-unix ${ADDITIONALVOLUMES} actc /opt/development/docker/actc/development.sh
